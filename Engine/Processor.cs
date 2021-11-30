acba7510dbb4def81318b0975c4c592dc2ea11fb
Youness KAFIA <younes.kafia-ext@coface.com>
2021-11-30 18:25:18 +0100

    created query

---
+++ Engine/Processor.cs
@@ -0,0 +1,12 @@
+namespace DXDebug.Engine
+{
+    public partial class Processor
+    {
+        public IQueryEntity Query;
+
+        public virtual void Update(EntityManager Manager)
+        {
+
+        }
+    }
+}
