78a3643a4180534194d47ccacab88ef429239c68
Youness KAFIA <32330908+ykafia@users.noreply.github.com>
2022-12-03 17:28:49 +0100

    vector 3 implemented

--- sources/SoftTouch/Numerics/Interfaces/ISoftVector3.cs
+++ sources/SoftTouch/Numerics/Interfaces/ISoftVector3.cs
@@ -9,7 +9,6 @@ internal interface ISoftVector3<T, Num> : ISoftVector2<T, Num>
     where Num : INumber<Num>
 {
     public Num Z {get;set;}
-
     public static abstract T New(Num x, Num y, Num z);
     public static new void Abs(in T value, out T result)
     {
@@ -34,118 +33,117 @@ internal interface ISoftVector3<T, Num> : ISoftVector2<T, Num>
             left.X + right.X,
             left.Y + right.Y,
             left.Z + right.Z
-
         );
     }
 
-    public static virtual void Clamp(in T value, in T min, in T max, out T result)
+    public static new void Clamp(in T value, in T min, in T max, out T result)
     {
         result = T.New(
             Num.MaxMagnitude(Num.MinMagnitude(value.X,max.X), min.X),
-            Num.MaxMagnitude(Num.MinMagnitude(value.Y,max.Y), min.Y)
+            Num.MaxMagnitude(Num.MinMagnitude(value.Y,max.Y), min.Y),
+            Num.MaxMagnitude(Num.MinMagnitude(value.Z,max.Z), min.Z)
         );
     }
-    public static abstract Num Distance(in T value);
-    public static virtual Num DistanceSquared(in T value)
+    public static new Num DistanceSquared(in T value)
     {
-        return value.X * value.X + value.Y * value.Y;
+        return value.X * value.X + value.Y * value.Y + value.Z * value.Z;
     }
-    public static virtual void Divide(Num scalar, in T value, out T result)
+    public static new void Divide(Num scalar, in T value, out T result)
     {
         result = T.New(
             scalar / value.X,
-            scalar / value.Y
+            scalar / value.Y,
+            scalar / value.Z
         );
     }
-    public static virtual void Divide(in T value, Num scalar, out T result)
+    public static new void Divide(in T value, Num scalar, out T result)
     {
         result = T.New(
             value.X / scalar,
-            value.Y / scalar
+            value.Y / scalar,
+            value.Z / scalar
         );
     }
-    public static virtual void Divide(in T left, in T right, out T result)
+    public static new void Divide(in T left, in T right, out T result)
     {
         result = T.New(
             left.X / right.X,
-            left.Y / right.Y
+            left.Y / right.Y,
+            left.Z / right.Z
         );
     }
-    public static virtual Num Dot(in T left, in T right)
+    public static new Num Dot(in T left, in T right)
     {
-        return left.X * right.X + left.Y * right.Y;
+        return left.X * right.X + left.Y * right.Y + left.Z * right.Z;
     }
-    public static virtual void Lerp(in T value1, in T value2, Num amount, out T result)
-    {
-        T.Subtract(value2, value1, out var sub);
-        T.Multiply(sub, amount, out var mul);
-        T.Add(value1, mul, out result);
-    }
-    public static virtual void Multiply(in T value, Num scalar, out T result)
+    public static new void Multiply(in T value, Num scalar, out T result)
     {
         result = T.New(
             value.X * scalar,
-            value.Y * scalar
+            value.Y * scalar,
+            value.Z * scalar
         );
     }
-    public static virtual void Multiply(in T left, in T right, out T result)
+    public static new void Multiply(in T left, in T right, out T result)
     {
         result = T.New(
             left.X * right.X,
-            left.Y * right.Y
+            left.Y * right.Y,
+            left.Z * right.Z
         );
     }
-    public static virtual void Negate(in T value, out T result)
+    public static new void Negate(in T value, out T result)
     {
         result = T.New(
             -value.X,
-            -value.Y
+            -value.Y,
+            -value.Z
         );
     }
-    public static virtual void Normalize(in T value, out T result)
+    public static new void Normalize(in T value, out T result)
     {
         var x = value.X;
         var y = value.Y;
+        var z = value.Z;
         result = T.New(
             x < -Num.One ? -Num.One : x > Num.One ? Num.One : x, 
-            x < -Num.One ? -Num.One : x > Num.One ? Num.One : y 
+            x < -Num.One ? -Num.One : x > Num.One ? Num.One : y,
+            z < -Num.One ? -Num.One : z > Num.One ? Num.One : z 
         );
     }
-    public static virtual void Reflect(in T vector, in T normal, out T result)
+    public static new void Reflect(in T vector, in T normal, out T result)
     {
         Num dot = T.Dot(vector,normal);
         result = T.New(
             vector.X - (Num.One + Num.One) * dot * normal.X,
-            vector.Y - (Num.One + Num.One) * dot * normal.Y
+            vector.Y - (Num.One + Num.One) * dot * normal.Y,
+            vector.Z - (Num.One + Num.One) * dot * normal.Z
         );
     }
-    public static abstract void SquareRoot(in T value, out T result);
     
-    public static virtual void Subtract(in T left, Num scalar, out T result)
+    public static new void Subtract(in T left, Num scalar, out T result)
     {
         result = T.New(
             left.X - scalar,
-            left.Y - scalar
+            left.Y - scalar,
+            left.Z - scalar
         );
     }
-    public static virtual void Subtract(Num scalar, in T left, out T result)
+    public static new void Subtract(Num scalar, in T left, out T result)
     {
         result = T.New(
             scalar - left.X,
-            scalar - left.Y
+            scalar - left.Y,
+            scalar - left.Z
         );
     }
-    public static virtual void Subtract(in T left, in T right, out T result)
+    public static new void Subtract(in T left, in T right, out T result)
     {
         result = T.New(
             left.X - right.X,
-            left.Y - right.Y
+            left.Y - right.Y,
+            left.Z - right.Z
         );
-    }
-
-    public static virtual void Transform(in T vector, in T transform, out T result)
-    {
-        T.Add(vector,transform, out result);
     }
 
     public abstract void Cross(T left, T right, out T result);
