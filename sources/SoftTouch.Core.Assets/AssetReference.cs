7e1dfe09581ea906f1c6322d72edaac1064d51a5
Youness KAFIA <kafia.youness@gmail.com>
2023-04-12 17:34:33 +0200

    correction asset reference

--- sources/SoftTouch.Core.Assets/AssetReference.cs
+++ sources/SoftTouch.Core.Assets/AssetReference.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using VYaml;
 using VYaml.Annotations;
+using VYaml.Emitter;
+using VYaml.Parser;
+using VYaml.Serialization;
 
 namespace SoftTouch.Core.Assets;
 
@@ -14,15 +17,5 @@ public partial struct AssetReference
     public Guid ID { get; set; }
 
     public TAsset Get<TAsset>() => (TAsset)AssetManager.GetOrCreate().LoadedAssets[ID];
+    public IAssetItem Get() => AssetManager.GetOrCreate().LoadedAssets[ID];
 }
-
-
-// [YamlObject]
-// public partial struct AssetReference<TAsset>
-//     where TAsset : IAssetItem
-// {
-//     public Guid ID { get; set; }
-
-//     [YamlIgnore]
-//     public TAsset Asset => (TAsset)AssetManager.GetOrCreate().LoadedAssets[ID];
-// }
